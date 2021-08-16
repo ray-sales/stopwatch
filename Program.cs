5a9d5e181621caf3ac743648a780c6da96eb29e5
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-16 18:07:07 -0300

    finish stopwatch

--- Program.cs
+++ Program.cs
@@ -7,12 +7,52 @@ namespace stopwatch
     {
         static void Main(string[] args)
         {
-            Start();
+            Menu();
         }
 
-        static void Start()
+        static void Menu()
         {
-            int time = 10;
+            Console.Clear();
+            Console.WriteLine("s - Seconds");
+            Console.WriteLine("m - Minutes");
+            Console.WriteLine("x - Exit");
+            Console.WriteLine("How much time do You want to count? EX.: 10s or 1m");
+
+            string data = Console.ReadLine().ToLower();
+
+            if (data == "x")
+            {
+                System.Environment.Exit(0);
+            }
+
+            char type = char.Parse(data.Substring((data.Length - 1), 1));
+            int time = int.Parse(data.Substring(0, (data.Length - 1)));
+
+            int multiplier = type == 'm' ? 60 : 1;
+
+
+
+            PreStart(time * multiplier);
+
+
+        }
+
+        static void PreStart(int time)
+        {
+            showPhrase("Ready...", 1000);
+            showPhrase("Set...", 1000);
+            showPhrase("Go!!!", 2500);
+            Start(time);
+        }
+
+        static void showPhrase(string text, int timeSleep)
+        {
+            Console.Clear();
+            Console.WriteLine(text);
+            Thread.Sleep(timeSleep);
+        }
+        static void Start(int time)
+        {
             int currentTime = 0;
 
             while (currentTime != time)
@@ -22,6 +62,9 @@ namespace stopwatch
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
             }
+
+            showPhrase("Finished timer", 2500);
+            Menu();
         }
     }
 }
