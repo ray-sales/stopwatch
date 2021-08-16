5cb7b5616c69638bb465cbcbe2e78a25632d2750
Ray Sales <78052891+ray-sales@users.noreply.github.com>
2021-08-16 17:24:54 -0300

    create start count

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                currentTime++;
+                Console.WriteLine(currentTime);
+            }
         }
     }
 }
